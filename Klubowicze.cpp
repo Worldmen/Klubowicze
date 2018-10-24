de287b127820b5b3c9a3d35897a7f2c2a0eac778
Marek Borzyszkowski <31738037+Worldmen@users.noreply.github.com>
2018-10-24 12:01:12 +0200

    Update Klubowicze.cpp

--- Klubowicze.cpp
+++ Klubowicze.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 #include <utility>
-
 using namespace std;
 
 void decToBin(bool **opinion, pair<int, int> *zeroOne, int m, int n) {
@@ -56,6 +55,22 @@ void condition(pair<int, int>*zeroOne, bool doable, bool* check, int n) {
 	}
 }
 
+bool doesExist(int n, int group, int position, pair<int, int>*zeroOne, bool **opinion, bool*check) {
+	pair<int, int>*zeroOneCurrent = new pair<int, int>[n];
+	for (int i = 0; i < n; i++) {
+		if (!check[i]) continue;
+		zeroOneCurrent[i].first = 0;
+		zeroOneCurrent[i].second = 0;
+		for (int j = position; j <= position + group; j++) {
+			if (opinion[j][i]) zeroOneCurrent[i].second++;
+			else zeroOneCurrent[i].first++;
+		}
+		if (zeroOne[i].first - zeroOneCurrent[i].first == 0 ||
+			zeroOne[i].second - zeroOneCurrent[i].second == 0) return 0;
+	}
+	return 1;
+}
+
 int main() {
 	int n, m;
 	cin >> n >> m;
@@ -70,7 +85,17 @@ int main() {
 	bool doable = true, *check = new bool[n];
 	condition(zeroOne, doable, check, n);
 	
-	
-	
+	int operacions, count = 0;
+	bool right;
+
+	if ((m % 10) % 2 == 1)
+		operacions = m / 2;
+	for (int i = 0; i < operacions; i++) {
+		for (int j = 0; j < m-i; j++) {
+			right = doesExist(n,i, j, zeroOne, opinion, check);
+			if (right) count++;
+		}
+	}
+	cout << count;
 	return 0;
 }
